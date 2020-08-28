29674c406bad49faaee93296729b43118ae72c16
asgaraliyev <asgaraliyev2001@gmail.com>
2020-08-28 22:20:40 +0400

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+    // var team_middle_width = $("#team-section .middle").width();
+    // var team_middle_height = $("#team-section .middle").height();
+    // console.log(team_middle_width,team_middle_height);
+    // $("#team-section").css({
+    //     "height":team_middle_height
+    // });
+    $("#team-section .member-shape").mouseenter(function(){
+        $(this).find(".about").slideDown("slow");
+    })
+    $("#team-section .member-shape").mouseleave(function(){
+        $(this).find(".about").slideUp("slow");
+    })
+ 
+    $( "#menu-list" ).on("click",function(){
+        $( "nav .menuList" ).fadeToggle("slow");
+    })
+    });
+
+var app = document.getElementById('typed');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Web Dizayn')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Blog saytları')
+    .pauseFor(2500)
+    .deleteAll()
+    .pauseFor(2500)
+    .typeString('Bura Baxilmalidi')
+    .start();
